a772a77baecdac92d5625e8018bcbe2f50f16c76
Tamillys <tamillyspantuza@gmail.com>
2020-08-02 20:42:02 -0300

    Changing URL backend

--- src/components/Button/button-index.js
+++ src/components/Button/button-index.js
@@ -7,18 +7,17 @@ const Button = styled.a`
     box-sizing: border-box;
     background-color: var(--primary);
     cursor: pointer;
-    padding: 8px 15px;
     padding: 8px 25px;
     font-style: normal;
     font-weight: bold;
     font-size: 18px;
     font-family: "Klavika-Light";
     outline: none;
-    border-radius: 2px;
     border-radius: 20px;
     text-decoration: none;
     display: inline-block;
     transition: all ease-in-out .1s;
+    
     &:hover,
     &:focus {
         background-color: #ea8774;
